13d14c614a78b4c707792333c263da406a2f03e7
Matthew Iannantuono <miannantuono12@gmail.com>
2025-05-21 20:45:03 -0400

    add starting screen

--- tutorly/app/(tabs)/index.tsx
+++ tutorly/app/(tabs)/index.tsx
@@ -1,75 +1,77 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
+import React from 'react'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+const start = () => {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Image source={require('../../assets/images/tutorlyLogo.png')} style={styles.logo}></Image>
+        <Text style={styles.title}>Tutorly</Text>
+      </View>
+      <Text style={styles.subParagraph}>Find study partners and peer tutors to excel with on Tutorly.</Text>
+      <TouchableOpacity style={styles.createAccount}>
+        <Text style={styles.createAccountText}>Create an Account</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.login}>
+        <Text style={styles.loginText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
+export default start
+
+const styles = StyleSheet.create({ 
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 30,
+    backgroundColor: 'white',
+  },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
+    gap: 20
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  logo: {
+    width: 100,
+    height: 100
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  title: {
+    color: '#075c3c',
+    fontSize: 56,
+    fontWeight: 'bold',
+    textAlign: 'center'
   },
-});
+  subParagraph: {
+    color: '#757575',
+    fontSize: 22,
+    textAlign: 'center',
+    marginVertical: 40,
+    marginHorizontal: 40
+  },
+  createAccount: {
+    backgroundColor: '#075c3c',
+    padding: 12,
+    borderRadius: 100,
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  createAccountText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  login: {
+    backgroundColor: 'white',
+    padding: 12,
+    borderRadius: 100,
+    alignItems: 'center',
+    borderColor: '#D9D9D9',
+    borderWidth: 1
+  },
+  loginText: {
+    color: '#075c3c',
+    fontSize: 16,
+  },
+})
